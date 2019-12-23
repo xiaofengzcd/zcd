33f30da07fdb88b9e11d99489097efe9d90dfdf7
xiaofengzcd <1849493899@qq.com>
2019-12-23 09:57:44 +0800

    提交单个文件

--- src/main/java/com/javen/git/GitTest.java
+++ src/main/java/com/javen/git/GitTest.java
@@ -7,7 +7,7 @@ package com.javen.collections;
  * @description: TODO
  * @date 2019/6/27 10:46
  */
-public class Test {
+public class GitTest {
    public void git(){
        System.out.println("测试git有无反应");
        System.out.println("再测试一次");
