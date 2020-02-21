ced7b0cc337d98faf46e447ddd80722309bf3323
xiaofengzcd <1849493899@qq.com>
2020-02-21 13:49:10 +0800

    关于github无法访问和push失败

--- src/main/java/com/zcd/git/GitTest.java
+++ src/main/java/com/zcd/git/GitTest.java
@@ -27,6 +27,29 @@ public class GitTest {
      * git push
      *
      * @link https://www.cnblogs.com/shenchanghui/p/7184101.html  参考链接
+     *
+     *
+     * 关于github被墙和push失败,有可能是代理的问题,网上一搜就是
+     * 可使用以下命令:
+     * git config --global http.proxy http://127.0.0.1:1080
+     * git config --global https.proxy http://127.0.0.1:1080
+     * 取消代理设置:
+     * git config --global --unset http.proxy
+     * git config --global --unset https.proxy
+     *
+     * 我踩过的坑:
+     * 上面的方法不行,无需设置,需更改windows下的hosts文件 地址：C:\Windows\System32\Drivers\etc
+     * 复制hosts文件到桌面,修改,再复制回原目录,替换
+     * 新增以下:
+     * #github
+     * 192.30.253.112 github.com
+     * 151.101.185.194 github.global.ssl.fastly.net
+     *
+     * 上面两个IP地址,可访问 http://github.com.ipaddress.com/
+     * http://github.global.ssl.fastly.net.ipaddress.com/ 获得
+     *
+     * @link https://jingyan.baidu.com/article/11c17a2cfb88d6f446e39d82.html
+     *
      */
    public void git(){
        System.out.println("测试git有无反应");
